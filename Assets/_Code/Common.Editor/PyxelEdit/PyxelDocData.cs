6b09c5a096f6dea57b5f7e335c30bc56e45b2709
Colin Bellino <bellino.colin@gmail.com>
2022-03-25 22:41:52 +0100

    Add a first draft of PyxelEdit importer

---
+++ Assets/_Code/Common.Editor/PyxelEdit/PyxelDocData.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace PyxelEdit
+{
+	[Serializable]
+	public class PyxelDocData
+	{
+		public string version;
+		public string name;
+		public Canvas canvas;
+		public Tileset tileset;
+
+		[Serializable]
+		public struct Canvas
+		{
+			public int width;
+			public int height;
+			public int tileHeight;
+			public int tileWidth;
+			public int currentLayerIndex;
+			public int numLayers;
+			[JsonConverter(typeof(ArrayMapConverter<Layer>))]
+			public Layer[] layers;
+		}
+
+		[Serializable]
+		public struct Layer
+		{
+			public string name;
+			public string blendMode;
+			public bool muted;
+			public bool hidden;
+			public bool collapsed;
+			public bool soloed;
+			public int alpha;
+			[JsonConverter(typeof(ArrayMapConverter<TileRef>))]
+			public TileRef[] tileRefs;
+			public string type;
+			public int parentIndex;
+		}
+
+		[Serializable]
+		public struct TileRef
+		{
+			public int index;
+			public bool flipX;
+			public int rot;
+		}
+
+		[Serializable]
+		public struct Tileset
+		{
+			public int tileHeight;
+			public bool fixedWidth;
+			public int tilesWide;
+			public bool fixedHeight;
+			public int numTiles;
+			public int tileWidth;
+		}
+	}
+
+	public class ArrayMapConverter<T> : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var t = JToken.FromObject(value);
+
+			if (t.Type != JTokenType.Array)
+			{
+				t.WriteTo(writer);
+			}
+			else
+			{
+				var array = (JArray)t;
+				var keys = array.Select((p, i) => i).ToList();
+
+				var o = new JObject();
+				foreach (var key in keys)
+				{
+					o.Add(new JProperty(key.ToString(), array[key]));
+				}
+
+				o.WriteTo(writer);
+			}
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			if (reader.TokenType == JsonToken.Null)
+			{
+				return string.Empty;
+			}
+			else if (reader.TokenType == JsonToken.String)
+			{
+				return serializer.Deserialize(reader, objectType);
+			}
+			else
+			{
+				var obj = JObject.Load(reader);
+				var array = new T[obj.Count];
+				foreach (var key in obj.Properties())
+				{
+					array[int.Parse(key.Name)] = obj[key.Name].ToObject<T>(serializer);
+				}
+				return array;
+			}
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return true;
+		}
+	}
+}
---
+++ Assets/_Code/Common.Editor/PyxelEdit/PyxelImporter.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace PyxelEdit
+{
+	[ScriptedImporter(1, "pyxel")]
+	public class PyxelImporter : ScriptedImporter
+	{
+		[SerializeField] [ReadOnly] public PyxelDocData _data;
+
+		public override void OnImportAsset(AssetImportContext ctx)
+		{
+			var entries = new Dictionary<string, byte[]>();
+			var layers = new List<string>();
+			var tiles = new List<string>();
+			var archiveName = Path.GetFileNameWithoutExtension(ctx.assetPath);
+
+			using (var archive = ZipFile.Open(ctx.assetPath, ZipArchiveMode.Read))
+			{
+				foreach (var entry in archive.Entries)
+				{
+					// UnityEngine.Debug.Log(entry.FullName);
+
+					if (entry.FullName.StartsWith("layer"))
+						layers.Add(entry.FullName);
+
+					if (entry.FullName.StartsWith("tile"))
+						tiles.Add(entry.FullName);
+
+					var stream = entry.Open();
+					var memoryStream = new MemoryStream();
+					stream.CopyTo(memoryStream);
+					stream.Close();
+
+					entries.Add(entry.FullName, memoryStream.GetBuffer());
+				}
+
+				foreach (var filename in layers)
+				{
+					var texture = new Texture2D(2, 2);
+					texture.LoadImage(entries[filename]);
+					var sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(texture.width / 2, texture.height / 2), texture.width);
+					sprite.name = archiveName + "_" + filename;
+
+					ctx.AddObjectToAsset(filename, sprite, sprite.texture);
+				}
+
+				// TODO: use TextureGenerationOutput output = TextureGenerator.GenerateTexture(settings, new Unity.Collections.NativeArray<Color32>(atlas.GetPixels32(), Unity.Collections.Allocator.Temp));
+				foreach (var filename in tiles)
+				{
+					var texture = new Texture2D(2, 2);
+					texture.LoadImage(entries[filename]);
+					var sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(texture.width / 2, texture.height / 2), texture.width);
+					sprite.name = archiveName + "_" + filename;
+
+					ctx.AddObjectToAsset(filename, sprite, sprite.texture);
+				}
+
+				{
+					var filename = "docData.json";
+					var dataString = Encoding.UTF8.GetString(entries[filename]);
+					_data = JsonConvert.DeserializeObject<PyxelDocData>(dataString);
+
+					var text = new TextAsset(dataString);
+					text.name = filename;
+
+					ctx.AddObjectToAsset(filename, text);
+					ctx.SetMainObject(text);
+				}
+
+			}
+		}
+	}
+
+	public class ReadOnlyAttribute : PropertyAttribute { }
+
+	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+	public class ReadOnlyDrawer : PropertyDrawer
+	{
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return EditorGUI.GetPropertyHeight(property, label, true);
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			GUI.enabled = false;
+			EditorGUI.PropertyField(position, property, label, true);
+			GUI.enabled = true;
+		}
+	}
+}

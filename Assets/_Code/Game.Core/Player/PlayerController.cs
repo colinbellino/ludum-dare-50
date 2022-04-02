34970cfee52f4bf037dd5e3fad1295525baad820
KyleWelfare <welfarek@sheridancollege.ca>
2022-04-02 12:53:19 -0400

    player dash

--- Assets/_Code/Game.Core/Player/PlayerController.cs
+++ Assets/_Code/Game.Core/Player/PlayerController.cs
@@ -11,6 +11,14 @@ namespace Game.Core {
         [SerializeField] private float xMoveSpeed;
         [SerializeField] private float yMoveSpeed;
         [SerializeField] private float diagonalXMoveSpeed;
+        [SerializeField] private float xDashSpeed;
+        [SerializeField] private float yDashSpeed;
+        [SerializeField] private float dashCooldown;
+        [SerializeField] private float dashDuration;
+
+         private float dashCounter;
+        private Vector2 RawMovementInput;
+        private bool isDashing;
 
         void Awake()
         {
@@ -19,10 +27,34 @@ namespace Game.Core {
             playerAnimator = GetComponent<Animator>();
         }
 
+        void Update() {
+            if (!isDashing) {
+                RawMovementInput = GameManager.Game.Controls.Gameplay.Move.ReadValue<Vector2>();
+            }
+           
+            GameManager.Game.Controls.Gameplay.Dash.performed += GetDashInput;
+
+            if (isDashing) {
+                if (dashCooldown - dashCounter > dashDuration) {
+                    isDashing = false;
+                }
+            }
+
+            if (dashCounter > 0) {
+                dashCounter -= Time.deltaTime;
+            }
+        }
+
         void FixedUpdate()
         {
-            Vector2 RawMovementInput = GameManager.Game.Controls.Gameplay.Move.ReadValue<Vector2>();
-            Debug.Log(RawMovementInput);
+            handleMovement(RawMovementInput);   
+
+            if (isDashing) {
+                playerRB.velocity = new Vector2(RawMovementInput.x * xDashSpeed, RawMovementInput.y * yDashSpeed);
+            }       
+        }
+
+        private void handleMovement(Vector2 RawMovementInput) {
             if (RawMovementInput != Vector2.zero) {
                 if (RawMovementInput.y == 0) {
                     playerRB.velocity = new Vector2(RawMovementInput.x * xMoveSpeed, 0);
@@ -37,6 +69,13 @@ namespace Game.Core {
                 }  
             } else {
                 playerRB.velocity = Vector2.zero;
+            }
+        }
+
+        private void GetDashInput(InputAction.CallbackContext context) {
+            if (context.action.triggered && dashCounter <= 0) {
+                isDashing = true;
+                dashCounter = dashCooldown;
             }
         }
     }

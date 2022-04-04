68b38821d7d234bcb6fbc80c6167fcb161eafac2
Colin Bellino <bellino.colin@gmail.com>
2022-04-05 01:06:46 +0200

    Dust particles when walking

--- Assets/_Code/Game.Core/Player/FootstepsAudio.cs
+++ Assets/_Code/Game.Core/Player/FootstepsAudio.cs
@@ -3,38 +3,40 @@ using Game.Core;
 
 public class FootstepsAudio : MonoBehaviour
 {
-    private bool isMoving;
-    [SerializeField] private float footStepSpeed;
-	 private Vector2 rawMovementInput;
-	 private PlayerController playerController;
+	private bool isMoving;
+	[SerializeField] private float footStepSpeed;
+	private Vector2 rawMovementInput;
+	private PlayerController playerController;
 
-    private void Awake()
-    {
+	private void Awake()
+	{
 		playerController = GetComponent<PlayerController>();
-        InvokeRepeating("CallFootsteps", 0, footStepSpeed);
-    }
+		InvokeRepeating("CallFootsteps", 0, footStepSpeed);
+	}
 
-    private void Update()
-    {
-		 rawMovementInput = GameManager.Game.Controls.Gameplay.Move.ReadValue<Vector2>();
-        if (rawMovementInput != Vector2.zero)
-        {
-            isMoving = true;
-        }
-        else
-        {
-            isMoving = false;
-        }
-    }
+	private void Update()
+	{
+		rawMovementInput = GameManager.Game.Controls.Gameplay.Move.ReadValue<Vector2>();
+		if (rawMovementInput != Vector2.zero)
+		{
+			isMoving = true;
+		}
+		else
+		{
+			isMoving = false;
+		}
+	}
 
-    private void OnDisable()
-    {
-        isMoving = false;
-    }
+	private void OnDisable()
+	{
+		isMoving = false;
+	}
 
-    private void CallFootsteps ()
-    {
-        if (isMoving && !playerController.getIsDashing())
-            AudioHelpers.PlayOneShot(GameManager.Game.Config.Footsteps, transform.position);
-    }
+	private void CallFootsteps()
+	{
+		if (isMoving && !playerController.getIsDashing())
+		{
+			AudioHelpers.PlayOneShot(GameManager.Game.Config.Footsteps, transform.position);
+		}
+	}
 }
